1c9b2c52bf8605ef5cb41f159909668f84057fac
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2022-04-05 12:42:39 +0200

    master

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+import random
 
 
 def t_setup():
@@ -13,6 +14,25 @@ def t_setup():
         turtle.penup()
         turtle.color(t_colors[turtles.index(turtle)])
         turtle.goto(x=-230, y=t_pos[turtles.index(turtle)])
+
+    return turtles
+
+
+def t_start_race(turtles, user_bet):
+
+    race_over = False
+
+    while not race_over:
+        for turtle in turtles:
+            turtle.forward(random.randint(1, 10))
+            t_pos = turtle.position()[0]
+            if t_pos >= 220:
+                if turtle.color()[0] == user_bet:
+                    print("You win!")
+                else:
+                    print(f"You lose. The {turtle.color()[0]} turtle is the winner, not the {user_bet} one.")
+                race_over = True
+
 
 
 
--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from functions import *
 
 screen = Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput(title="user_bet", prompt="Which turtle will win the race? Enter a color: ")
-t_setup()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+user_bet = screen.textinput(title="user_bet", prompt="Which turtle will win the race? Enter a color: ").lower()
+turtles = t_setup()
+t_start_race(turtles, user_bet)
 
 screen.exitonclick()
